8645823d7c91a0ebc7a3e74edcaf25725feaeab9
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-22 09:39:00 +0330

    add chalk

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import express from 'express'
 import methodOverride from 'method-override'
 import cors from 'cors'
 import helmet from 'helmet'
+import chalk from 'chalk'
 import dotenv from 'dotenv'
 import dotenvExpand from 'dotenv-expand'
 /* -------------------------------------------------------------------------- */
@@ -19,6 +20,11 @@ dotenvExpand.expand(env)
 const app = express()
 const server = http.createServer(app)
 const port = process.env.PORT || '3000'
+
+const loveStyle = chalk.hex('#f6009b')
+const successStyle = chalk.hex('#00FF00')
+const warnStyle = chalk.hex('#FFFF00').bold
+const errStyle = chalk.hex('#FF0000').bold
 
 /* -------------------------------------------------------------------------- */
 app.set('port', port)
@@ -39,7 +45,7 @@ app.use('*', (error: AppError, _, res, __) => {
       data: null,
     })
   else {
-    console.log('Unknown type error:', error)
+    console.error(errStyle('Unknown type error:'), error)
     return {
       success: false,
       error,
@@ -51,4 +57,6 @@ app.use('*', (error: AppError, _, res, __) => {
 
 server
   .listen(app.get('port'))
-  .on('listening', () => console.log(`🧩 HTTP:\t ${app.get('server_address')}`))
+  .on('listening', () =>
+    console.log(`${loveStyle('HTTP')}:\t ${successStyle(app.get('server_address'))}`)
+  )
