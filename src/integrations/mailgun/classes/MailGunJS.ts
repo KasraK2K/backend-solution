b7d275a9978fd2b668619152cd4bf6eab30552c7
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-23 16:54:25 +0330

    create MailGun integration

---
+++ src/integrations/mailgun/classes/MailGunJS.ts
@@ -0,0 +1,24 @@
+import { Message } from ".."
+import Mailgun from "mailgun.js"
+import formData from "form-data"
+
+const mailgun = new Mailgun(formData)
+
+class MailGunJS {
+  private certificate = {
+    key: "_key_",
+    url: "https://api.eu.mailgun.net",
+    from: "Business Name <info@business_email.com>",
+    domain: "mailer.registered_domain-for_mailgun.com",
+  }
+
+  public client = mailgun.client({
+    username: "api",
+    key: process.env.MAILGUN_API_KEY || this.certificate.key,
+    url: this.certificate.url,
+  })
+
+  public message = new Message(this, this.certificate)
+}
+
+export default MailGunJS
