8f317e63846d4dd8d6a72853ba26b20b401b2fa1
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-29 19:32:05 +0330

    fix filename and path when using webpack

--- src/common/helpers/error/errorFilePath.ts
+++ src/common/helpers/error/errorFilePath.ts
@@ -4,20 +4,18 @@ import AppError from './AppError'
 const errorFilePath = (error: unknown | Error | AppError) => {
   if ((error instanceof Error || error instanceof AppError) && error.stack) {
     const stack = error.stack.split('\n').map((line) => line.trim())
-    let line: string = stack[2]
-
-    // error instanceof AppError || error instanceof Error ? (line = stack[1]) : (line = stack[2])
+    let line: string = stack[2].trim()
 
     const hasParentheses = line.endsWith(')')
     let file_path: string
     let file_name: string
 
     if (hasParentheses) {
-      file_path = line.slice(line.indexOf('(') + 1 + process.cwd().length, line.length - 1)
-      file_name = line.slice(line.lastIndexOf('/') + 1, line.indexOf(':'))
+      file_path = line.slice(line.indexOf('/src'), line.length - 1)
+      file_name = line.slice(line.lastIndexOf('/') + 1, line.indexOf('?'))
     } else {
-      file_path = line.slice(line.indexOf('/') + process.cwd().length)
-      file_name = line.slice(line.lastIndexOf('/') + 1, line.indexOf(':'))
+      file_path = line.slice(line.indexOf('/src'))
+      file_name = line.slice(line.lastIndexOf('/') + 1, line.indexOf('?'))
     }
 
     return { file_name, file_path }
